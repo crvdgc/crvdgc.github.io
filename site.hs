d9c45ba3659432b807039843131cd00cd912c0fb
crvdgc <ubikium@gmail.com>
2022-07-25 16:35:28 +0900

    add feed and 404

--- site.hs
+++ site.hs
@@ -4,70 +4,99 @@
 import qualified GHC.IO.Encoding as E
 import Hakyll
 
+import Debug.Trace (traceShowId)
+
 --------------------------------------------------------------------------------
 main :: IO ()
 main = do
   E.setLocaleEncoding E.utf8
   hakyllWith config $ do
-    match "images/*" $ do
+    match ("images/*"
+            .||. "assets/*"
+            .||. "css/*"
+            .||. "css/font/*"
+            .||. "google85266e65053758c9.html"
+            .||. "favicon.ico"
+          ) $ do
       route idRoute
       compile copyFileCompiler
-    match "assets/*" $ do
-      route idRoute
-      compile copyFileCompiler
-    match "css/*" $ do
-      route idRoute
-      compile compressCssCompiler
-    match "css/font/*" $ do
-      route idRoute
-      compile copyFileCompiler
-    match (fromList ["about.md"]) $ do
-      route $ setExtension "html"
-      compile $
-        pandocCompiler >>= loadAndApplyTemplate "templates/default.html" siteCtx
-          >>= relativizeUrls
+
+    -- posts
     match "portfolio/*" $ do
       route $ setExtension "html"
       compile $
-        pandocCompiler >>= loadAndApplyTemplate "templates/post.html" postCtx
+        pandocCompiler
+          >>= loadAndApplyTemplate "templates/post.html" postCtx
+          >>= saveSnapshot "content"
           >>= loadAndApplyTemplate "templates/default.html" postCtx
+          >>= relativizeUrls
+
+    -- meta
+    match "index.html" $ do
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll "portfolio/*"
+        let ctx = indexCtx posts
+        getResourceBody
+          >>= applyAsTemplate ctx
+          >>= loadAndApplyTemplate "templates/default.html" ctx
+          >>= relativizeUrls
+    match "about.md" $ do
+      route $ setExtension "html"
+      compile $
+        pandocCompiler
+          >>= loadAndApplyTemplate "templates/default.html" siteCtx
           >>= relativizeUrls
     create ["archive.html"] $ do
       route idRoute
       compile $ do
         posts <- recentFirst =<< loadAll "portfolio/*"
-        let archiveCtx =
-              listField "posts" postCtx (return posts)
-                `mappend` constField "title" "Archives"
-                `mappend` siteCtx
-        makeItem "" >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-          >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        let ctx = archiveCtx posts
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/archive.html" ctx
+          >>= loadAndApplyTemplate "templates/default.html" ctx
           >>= relativizeUrls
-    match "index.html" $ do
+    create ["404.html"] $ do
       route idRoute
       compile $ do
-        posts <- recentFirst =<< loadAll "portfolio/*"
-        let indexCtx =
-              listField "posts" postCtx (return posts)
-                `mappend` constField "title" "Home"
-                `mappend` siteCtx
-        getResourceBody >>= applyAsTemplate indexCtx
-          >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/404.html" notFoundCtx
+          >>= loadAndApplyTemplate "templates/default.html" notFoundCtx
           >>= relativizeUrls
-    match "google85266e65053758c9.html" $ do
-      route idRoute
-      compile copyFileCompiler
-    match "favicon.ico" $ do
-      route idRoute
-      compile copyFileCompiler
     match "templates/*" $ compile templateCompiler
 
---------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx = dateField "date" "%Y-%m-%d" `mappend` siteCtx
+    -- feed
+    create ["atom.xml"] $ do
+        route idRoute
+        compile (feedCompiler renderAtom)
+    create ["rss.xml"] $ do
+        route idRoute
+        compile (feedCompiler renderRss)
 
+--------------------------------------------------------------------------------
 siteCtx :: Context String
-siteCtx = activeClassField `mappend` defaultContext
+siteCtx = activeClassField <> defaultContext
+
+postCtx :: Context String
+postCtx = dateField "date" "%Y-%m-%d" <> siteCtx
+
+feedCtx :: Context String
+feedCtx = bodyField "description" <> postCtx
+
+listWithTitleCtx :: String -> [Item String] -> Context String
+listWithTitleCtx title posts =
+  listField "posts" postCtx (pure posts)
+    <> constField "title" title
+    <> siteCtx
+
+indexCtx :: [Item String] -> Context String
+indexCtx = listWithTitleCtx "Home"
+
+archiveCtx :: [Item String] -> Context String
+archiveCtx = listWithTitleCtx "Archives"
+
+notFoundCtx :: Context String
+notFoundCtx = constField "title" "Not Found" <> postCtx
 
 -- https://groups.google.com/forum/#!searchin/hakyll/if$20class/hakyll/WGDYRa3Xg-w/nMJZ4KT8OZUJ
 activeClassField :: Context a
@@ -82,3 +111,29 @@ activeClassField =
 -- change output folder to docs for Github pages
 config :: Configuration
 config = defaultConfiguration {destinationDirectory = "docs"}
+
+-- * Feeds
+-- https://jaspervdj.be/hakyll/tutorials/05-snapshots-feeds.html
+-- and
+-- https://robertwpearce.com/hakyll-pt-3-generating-rss-and-atom-xml-feeds.html
+
+feedConfig :: FeedConfiguration
+feedConfig = FeedConfiguration
+  { feedTitle = "Crvdgc 的个人文集"
+  , feedDescription = "不定期发布自己创作的小说，以及一些关于哲学、计算机科学、小说、电影和游戏的思考。"
+  , feedAuthorName = "crvdgc"
+  , feedAuthorEmail = "ubikium@gmail.com"
+  , feedRoot = "https://crvdgc.github.io"
+  }
+
+type FeedRenderer =
+  FeedConfiguration
+  -> Context String
+  -> [Item String]
+  -> Compiler (Item String)
+
+feedCompiler :: FeedRenderer -> Compiler (Item String)
+feedCompiler renderer =
+  renderer feedConfig feedCtx
+    =<< fmap (take 10) . recentFirst
+    =<< loadAllSnapshots "portfolio/*" "content"
