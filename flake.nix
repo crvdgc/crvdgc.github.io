b97da9a76c0401b400a5bfaa8ac7f5904914845f
crvdgc <ubikium@gmail.com>
2023-03-26 23:41:42 +0100

    non hakyll-flake build for macos

--- flake.nix
+++ flake.nix
@@ -1,21 +1,22 @@
 {
   description = "site";
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
-  inputs.hakyll-flakes.url = "github:Radvendii/hakyll-flakes";
+  # inputs.hakyll-flakes.url = "github:Radvendii/hakyll-flakes";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, hakyll-flakes, flake-utils, nixpkgs }:
+  outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (
       system:
-      hakyll-flakes.lib.mkAllOutputs {
-        inherit system;
-        name = "site";
-        src = ./.;
-        websiteBuildInputs = with nixpkgs.legacyPackages.${system}; [
-          # rubber
-          # texlive.combined.scheme-full
-          # poppler_utils
-        ];
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ zlib pkg-config ];
+          shellHook = ''
+            PKG_CONFIG_PATH="${pkgs.zlib}:$PKG_CONFIG_PATH"
+          '';
+        };
       }
     );
 }
